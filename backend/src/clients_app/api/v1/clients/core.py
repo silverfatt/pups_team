2606cc197e496f61d9cedd76040d8e34949a3cf6
silverfatt <silvestr.fat@mail.ru>
2024-12-21 10:26:29 +0300

    minor fixes

--- backend/src/clients_app/api/v1/clients/core.py
+++ backend/src/clients_app/api/v1/clients/core.py
@@ -4,17 +4,20 @@ from asyncpg import Pool
 #     query = "SELECT * FROM default.NewTable"
 #     return client.execute(query)
 
+rating_map = {0: "Негативные", 1: "Нейтральные", 2: "Положительные"}
+
 
 async def get_grade_distribution_of_records(pool: Pool) -> dict:
     query = "SELECT rating_value, COUNT(*) FROM clients GROUP BY rating_value ORDER BY rating_value"
     async with pool.acquire() as connection:
         async with connection.transaction():
             res = await connection.fetch(query)
-    return {str(item[0]): item[1] for item in res} if res else {}  # type: ignore
+    return {rating_map[item[0]]: item[1] for item in res} if res else {}
 
 
 async def get_date_distribution_of_records(pool: Pool) -> dict:
-    query = "SELECT comment_date, COUNT(*) FROM clients GROUP BY comment_date ORDER BY comment_date"
+    query = "SELECT  EXTRACT(month FROM comment_date),COUNT(*) FROM clients GROUP BY EXTRACT(month FROM comment_date) ORDER BY EXTRACT(month FROM comment_date)"
+
     async with pool.acquire() as connection:
         async with connection.transaction():
             res = await connection.fetch(
